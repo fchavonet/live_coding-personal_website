b2e357c142a2a6da911dfe267ff2c35da84afb4b
fchavonet <fchavonet@mail.fr>
2025-09-10 16:23:44 +0200

    feat: create dynamic behavior for carousel data.

--- script.js
+++ script.js
@@ -105,3 +105,70 @@ anchors.forEach(function (anchor) {
 		window.dispatchEvent(new Event("scroll"));
 	});
 });
+
+
+/****************************
+* DATA FOR DYNAMIC CAROUSEL *
+****************************/
+
+const projects = [
+	{
+		title: "Holberton Survivors",
+		description: 'Dans le cadre de ma formation à la Holberton School, j’ai co-développé avec <a href="https://github.com/SpStigma" target="_blank">Yoann Rivet</a> un jeu vidéo inspiré de <a href="https://store.steampowered.com/app/1794680/Vampire_Survivors/" target="_blank">Vampire Survivors</a>, réalisé en deux mois avec Unity en C#, sans connaissance préalable de ces outils. Présenté en anglais devant un jury de professionnels lors de notre soutenance finale, ce projet a mobilisé nos compétences en développement logiciel, gestion de projet et travail d’équipe, contribuant à l’obtention de notre diplôme.',
+		image: "./assets/images/carousel/works-holberton_survivors.webp",
+		link: "https://www.holbertonsurvivors.com/",
+	},
+	{
+		title: "GitHub README Builder",
+		description: 'Ce projet est une application web interactive qui permet aux utilisateurs de composer, prévisualiser et exporter des fichiers README.md pour <a href="https://github.com/" target="_blank">GitHub</a>. Elle propose un éditeur <a href="https://fr.wikipedia.org/wiki/Markdown" target="_blank">Markdown</a> en temps réel, des modules de contenu prédéfinis, un générateur de badges personnalisés basé sur ceux de <a href="https://shields.io/" target="_blank">Shields.io</a> et une prévisualisation instantanée au format et dans le style d\'affichage de GitHub (même CSS).',
+		image: "./assets/images/carousel/works-github_readme_builder.webp",
+		link: "https://fchavonet.github.io/web-github_readme_builder/"
+	},
+	{
+		title: "Flip Book",
+		description: 'Un simple flipbook réalisé en HTML et CSS. Son design essaie de reproduire la même expérience que celle du feuilletage d’un petit livre. Grâce aux animations et transitions CSS, le flipbook offre à l’utilisateur une navigation fluide, alliant fonctionnalité et esthétisme.',
+		image: "./assets/images/carousel/works-flip_book.webp",
+		link: "https://fchavonet.github.io/web-flip_book/"
+	},
+]
+
+/****************************
+* DYNAMIC CAROUSEL BEHAVIOR *
+****************************/
+
+const worksTitle = document.getElementById("works-title");
+const carouselCaptionText = document.getElementById("carousel-caption-text");
+const carouselInner = document.querySelector("#works-carousel .carousel-inner");
+const carouselProjectLink = document.getElementById("carousel-project-link");
+
+worksTitle.textContent = projects[0].title;
+carouselCaptionText.innerHTML = projects[0].description;
+carouselProjectLink.href = projects[0].link;
+
+// Create each carousel item dynamically.
+projects.forEach(function (project, index) {
+	const item = document.createElement("div");
+	item.classList.add("carousel-item");
+
+	if (index === 0) {
+		item.classList.add("active");
+	}
+
+	const img = document.createElement("img");
+	img.classList.add("w-100", "d-block");
+	img.src = project.image;
+	img.alt = "Capture d'écran du projet " + project.title;
+
+	item.appendChild(img);
+	carouselInner.appendChild(item);
+});
+
+//
+const worksCarousel = document.getElementById("works-carousel");
+
+worksCarousel.addEventListener("slid.bs.carousel", function (event) {
+	const index = event.to;
+	worksTitle.textContent = projects[index].title;
+	carouselCaptionText.innerHTML = projects[index].description;
+	carouselProjectLink.href = projects[index].link;
+});
